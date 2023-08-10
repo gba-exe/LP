827df45fb01ba669db0801e539517e5bc5dc124a
gba-exe <gabriel.baraujo@sptech.school>
2023-08-10 11:22:27 -0300

    feat(): operadores ternários e operadores condicionais

--- fundamento01/src/main/java/exe/gba/Operador.java
+++ fundamento01/src/main/java/exe/gba/Operador.java
@@ -52,5 +52,64 @@ public class Operador {
             System.out.println("Não é igual");
         }
 
+        Boolean isIgual = numero1 == numero2;
+
+        //operadores ternários existem da mesma forma que no JS
+
+        // ou seja
+
+        // essa variável é igual a
+        String mensagem =
+                numero1 == numero2 ? "é igual" : "não é igual";
+
+        // essa cadeia de if else
+        if (isIgual){
+            System.out.println("É igual");
+        } else {
+            System.out.println("Não é igual");
+        }
+
+        // o if para boolean é mais facil, não precisamos usar o ==, como por exemplo
+
+        if(isIgual){
+            System.out.println("condicional true");
+        }
+
+        if(!isIgual){
+            System.out.println("condicional false");
+        }
+
+        /*
+            outro operador condicional é o switch case
+
+         */
+        Integer numeroOpcao = 2;
+
+        switch (numeroOpcao){ // aqui dizemos a variavel que queremos observar
+            case 1: // caso a variavel seja igual a 1
+                System.out.println("usuario escolheu 1");// printamos isso
+            break;
+
+            case 2: // caso a variavel seja igual a 2
+                System.out.println("usuario escolheu 2");// printamos isso
+                break;
+
+            case 3: // caso a variavel seja igual a 3
+                System.out.println("usuario escolheu 3");// printamos isso
+                break;
+
+            case 4: // caso a variavel seja igual a 4
+                System.out.println("usuario escolheu 4");// printamos isso
+                break;
+
+            default: // é como se fosse um else, caso não encontre a comparação acima, ele solta o q tiver no default
+                System.out.println("opção não encontrada");
+        }
+        //como se fosse if e else if, porém de uma vez só, ele compara todos, um if para várias condições basicamente
+
+        /*
+        case = if/else if
+        default = else
+         */
     }
 }
